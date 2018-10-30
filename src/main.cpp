bdf72d9f8b0b97186839ec7f074873a60270c02d
Boguslaw Boczkowski <bboczkowski@openskydata.com>
2018-10-30 22:18:38 +0100

    added functionality for LED

--- src/main.cpp
+++ src/main.cpp
@@ -17,12 +17,14 @@ WebSocketsClient webSocket;
 #define MESSAGE_INTERVAL 2000
 #define DHTPIN 5
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+#define LED 2
 DHT dht(DHTPIN, DHTTYPE);
 //#define HEARTBEAT_INTERVAL 25000
 
 uint64_t messageTimestamp = 0;
 uint64_t heartbeatTimestamp = 0;
 bool isConnected = false;
+bool ledStatus = !false;
 
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
     switch(type) {
@@ -40,10 +42,25 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
             }
             break;
         case WStype_TEXT:
+          {
             USE_SERIAL.printf("[WSc] get text: %s\n", payload);
+            //digitalWrite(LED, ledStatus);
+            //ledStatus = !ledStatus;
+            /*
+            String text = ((const char *)&payload[0]);
+            int pos = text.indexOf('{');
+            String json = text.substring(pos,text.length()-1);
 
-			// send message to server
-			// webSocket.sendTXT("message here");
+        const size_t BUFFER_SIZE =
+             JSON_OBJECT_SIZE(10)
+             + 200;
+         DynamicJsonBuffer jsonBuffer(BUFFER_SIZE);
+
+         JsonObject& root = jsonBuffer.parseObject(json);
+         String msg = root["Ledi"];
+         */
+         //USE_SERIAL.printf("Ledi: %s\n", msg);
+          }
             break;
         case WStype_BIN:
             USE_SERIAL.printf("[WSc] get binary length: %u\n", length);
@@ -55,7 +72,7 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
     }
 }
 
-void CreateJsonPayload(String &jsonString, float h, float t, float f, float hif, float hic)
+void CreateJsonPayload(String &jsonString, float h, float t, float f, float hif, float hic, bool ledStat)
 {
   String hum = String(h) + "%";
   String temp = String(t) + " *C " + String(f) + " *F";
@@ -65,12 +82,15 @@ void CreateJsonPayload(String &jsonString, float h, float t, float f, float hif,
   root["Humidity"] = hum;
   root["Temperature"] = temp;
   root["Heat index"] = heatIndex;
+  root["Led"] = ledStat;
   String buffer;
   root.printTo(buffer);
   jsonString = "42[\"tempSensor\","+buffer+"]";
 }
 
 void setup() {
+    pinMode(LED, OUTPUT);
+    digitalWrite(LED, ledStatus);
     // USE_SERIAL.begin(921600);
     USE_SERIAL.begin(9600);
     //Serial.setDebugOutput(true);
@@ -120,7 +140,7 @@ void loop() {
               float hic = dht.computeHeatIndex(t, h, false);
 
               String jsonString;
-              CreateJsonPayload(jsonString, h, t, f, hif, hic);
+              CreateJsonPayload(jsonString, h, t, f, hif, hic, ledStatus);
               // example socket.io message with type "messageType" and JSON payload
               webSocket.sendTXT(jsonString);
             }
